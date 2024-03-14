b273db4f758dc2a42913c1a8045f09827fb84739
Ravikumarmsccs <139948161+Ravikumarmsccs@users.noreply.github.com>
2024-03-14 10:05:34 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+import { Card } from "react-bootstrap";
+// import Bank from "./logo.jpg";
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div >
+    
+      
+
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// //import Deposit from './atmcreate.js';
+import reportWebVitals from './reportWebVitals';
+// import Mark from './player.js';
+// //import Deposit from './atmcreate.js';
+// //import TicTacToe from './win.js';
+// //import Todos from './date.js';
+// //import Navigation_page from './nav.js'; 
+// import ShoppingCart from './shopping cart.js';
+// //import Navigation_page from './nav.js';
+// //import BadBank from './badbank.js';
+// //import LoginPage from './login.js';
+// import SignupForm from './data.js';
+// import App from './App.js';
+// //import DepositWithdraw from './deposit.js';
+// // import NavigationBar from './Bad.js';
+// //import App from './stocks.js';
+// import pizza from './pizza.js';
+// import Name from './assign3.3.js';
+// //import App from './error.js';
+// import Todos from './date.js';
+// import Myelement from './elements.js';
+// import Number from './ATM.js';
+// import Pagination from './Pagination.js';
+// import TicTac from './player.js';
+// import Player from './player.js';
+// import TicTacToe from './win.js';
+// import Navigation_page from './nav.js';
+// //import App from './book.js';
+// //import Web_combonent from './books.js';
+
+
+import NavBar from "./navbar";
+import App from "./App7";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <App/>
+     
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
